d9f7260f54fe8f5eb915236d3ec6cf5adeb27993
Ky Anh Vo <tommyvo0406@gmail.com>
2025-09-07 10:27:55 +0000

    added... something to id3

--- id3/main.c
+++ id3/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
 
 
 struct training_data {
@@ -22,12 +23,20 @@ struct node {
     size_t * left, * right;
 };
 
+/*****************************************************
+ * AUXILIARY FUNCTIONS *******************************
+ ****************************************************/
+
 /**
  * @brief setup param_count, param_names, training_set
  *
  * @param fp file pointer to training data file
  */
 void setup(FILE *);
+
+/*****************************************************
+ * ID3-RELATED FUNCTIONS *****************************
+ ****************************************************/
 
 /**
  * @brief partition subarray based on a certain parameter `param` for 0, 1, 2.
@@ -44,8 +53,36 @@ void setup(FILE *);
  * @param param parameter to partition
  * @param b1 points to index later stores starting location of 2nd partition
  * @param b2 points to index later stores starting location of 3rd partition
+ *
+ * @note if b1 == 0 then there is no first section. if b1 == b2 then there is
+ * no 2nd section. if  b2 == right + 1 then there is no 3rd section.
  */
 void ternary_partition(uint8_t *, uint8_t, size_t, size_t, size_t *, size_t *);
+
+/**
+ * @brief given p, calculate p * lg(p)
+ *
+ * @param p double value as a probability
+ * @return p * log_2(p)
+ */
+double entropy_term(double);
+
+/**
+ * @brief calculate entropy of a node.
+ *
+ * Given a node's left and right parameters and a parameter to split decisions,
+ * calculate the potential entropy of next level given that this parameter is chosen.
+ *
+ * @param arr the array that is the training set
+ * @param param the param that we split
+ * @param left first index of subarray
+ * @right last last index of subarray + 1
+ */
+double entropy(uint8_t *, uint8_t, size_t, size_t);
+
+/*****************************************************
+ * MAIN FUNCTION *************************************
+ ****************************************************/
 
 int main(int argc, char** argv) {
     FILE * fp = fopen(argv[1], "r");
@@ -56,6 +93,10 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+/*****************************************************
+ * IMPLEMENTATION ************************************
+ ****************************************************/
+
 uint8_t is_whitespace(char c) {
     return c == ' ' ||
         c == '\t' ||
@@ -63,15 +104,30 @@ uint8_t is_whitespace(char c) {
         c == '\r';
 }
 
+uint8_t is_empty_line(char * line) {
+    uint8_t i = 0;
+    while (line[i])
+        if (!is_whitespace(line[i]))
+            return 0;
+    return 1;
+}
+
 void setup(FILE * fptr) {
     char line[100]  = {0};
     char tmp[100]   = {0};
 
     // read names and count inputs
-    fgets(line, 100, fptr);
+    
+    // skip line that does nothing
+    do {
+        fgets(line, 100, fptr);
+    } while (is_empty_line(line));
+    
+    // read param names
     uint8_t i = 0, tmp_ind = 0;
     while (line[i]) {
         if (is_whitespace(line[i])) {
+            if (tmp_ind == 0) ; // do nothing
             if (strcmp(tmp, "class") == 0) {
                 // do nothing
             }
@@ -91,4 +147,15 @@ void setup(FILE * fptr) {
         }
         i++;
     }
+    
+    // 
+}
+
+double entropy_term(double p) {
+    if (p == 0) return 0;
+    else return p * log2(p);
+}
+
+double entropy(uint8_t * arr, uint8_t param, size_t left, size_t right) {
+    double H1 = 0, H2 = 0, H3 = 0, amount1 = 0, amount2 = 0, amount3 = 0, total = 0;
 }
